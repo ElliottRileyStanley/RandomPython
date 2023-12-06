3a8a0822b9cf5b7297896b8b241f7e4b31fc0c98
ElliottRileyStanley <27elliottstanley@usd266.com>
2023-12-06 10:10:45 -0600

    anagram

---
+++ Anagram/solver.py
@@ -0,0 +1,8 @@
+word_list = open('Anagram\organized_dictionary.text').readlines()
+
+letters = ["k", "b", "k", "d", "e", "w", "k", "i", "a"]
+
+for word in word_list:
+    for letter in word:
+        if letter in 
+print(word_list[0])
