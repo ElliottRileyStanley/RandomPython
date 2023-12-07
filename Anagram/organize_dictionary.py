6513206e1c45845a344b4de519015e3be09f8a00
ElliottRileyStanley <27elliottstanley@usd266.com>
2023-12-06 18:42:34 -0600

    test

--- Anagram/organize_dictionary.py
+++ Anagram/organize_dictionary.py
@@ -1,6 +1,5 @@
 old = open('Anagram\dictionary_defs.txt').readlines()
 new = open('Anagram\organized_dictionary.text', "a")
-print (len(old[0]))
 for word in old:
     word = word.rsplit("\t")[0]
     if len(word) > 3 and len(word) < 10:
--- Anagram/solver.py
+++ Anagram/solver.py
@@ -2,7 +2,13 @@ word_list = open('Anagram\organized_dictionary.text').readlines()
 
 letters = ["k", "b", "k", "d", "e", "w", "k", "i", "a"]
 
-for word in word_list:
+def check_word(word, letters):
     for letter in word:
-        pass
-print(word_list[0])
+        if not letter in letters:
+            return False
+    return True
+
+for word in word_list:
+    if check_word(word, letters):
+        print(word)
+
