42d856c88daec79317bd520b1f0a4ab8294ae72e
ElliottRileyStanley <27elliottstanley@usd266.com>
2023-12-07 10:12:39 -0600

    Message

--- Anagram/solver.py
+++ Anagram/solver.py
@@ -1,14 +1,25 @@
+import pyautogui
+import time
+
 word_list = open(r"Anagram\organized_dictionary.text").readlines()
 
+game_type = input("Game 1/2")
+
 def check_word(word):
-    letters = "sqnlugerw"
+    letters = "aeydrdedx"
+    if game_type == "2" and letters[4] not in word:
+        return False
     for letter in word:
         if letter not in letters:
             return False
         letters = letters.replace(letter, "", 1)
+    pyautogui.write(word)
+    pyautogui.press('enter')
     return True
 
 result = []
+
+time.sleep(3)
 
 for word in word_list:
     word = word[0:len(word)-1]
