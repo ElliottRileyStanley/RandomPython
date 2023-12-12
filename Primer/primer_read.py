25adb45fbddf3b824ac1c041f11e47dbac2c9b4d
ElliottRileyStanley <27elliottstanley@usd266.com>
2023-12-11 20:59:53 -0600

    Have fun future Elliott

--- Primer/primer_read.py
+++ Primer/primer_read.py
@@ -3,10 +3,11 @@ import numpy
 import random
 import time
 
-strat = open(r'Primer\AI\newstrat.text').readlines()
+strat = open(r'Primer\mathstrat.text').readlines()
 heads = 1
 tails = 0
-flips = 67
+flips = 100
+just_accused = 1
 
 def calibrate_mouse():
     input("Press enter when you are in position")
@@ -20,10 +21,11 @@ def has_changed(startx, endx, starty, endy):
     return False
 
 def choiceaoeu(heads, tails, flips, strat):
-    if heads + tails > 20 or flips == 0:
+    if heads + tails > 15:
         return (random.randrange(1,2))
     else:
-        return strat[int(((21*22/2)+1)*min(flips, 20)+((heads+tails)*(heads+tails+1)/2)+tails)][0]
+        print(int(((16*17/2)+1)*min(flips, 14)+((heads+tails)*(heads+tails+1)/2)+tails))
+        return strat[int(((16*17/2)+1)*min(flips, 15)+((heads+tails)*(heads+tails+1)/2)+tails)][0]
     
 
 heads1x, heads1y = calibrate_mouse()
@@ -48,18 +50,42 @@ while flips >=0:
     elif int(choice) == 2:
         pyautogui.click(cheatx, cheaty)
         time.sleep(3)
-    im_new = pyautogui.screenshot()
 
+    im_new = pyautogui.screenshot()
+    
     if int(choice) == 0:
-        if has_changed(heads1x, heads2x, heads1y, heads2y):
-            heads += 1
+        if just_accused == 1:
+            lefthead = 0
+            righthead = 0
+            lefttail = 0
+            righttail = 0
+            for x in range(heads1x, heads2x):
+                if sum(im_prev.convert('RGB').getpixel([x,round((heads1y+heads2y)/2, 0)])) > 500:
+                    if lefthead == 0:
+                        lefthead = x
+                    righthead = x
+            for x in range(tails1x, tails2x):
+                if sum(im_prev.convert('RGB').getpixel([x,round((tails1y+tails2y)/2, 0)])) > 500:
+                    if lefttail == 0:
+                        lefttail = x
+                    righttail = x
+            if righthead-lefthead > righttail-lefttail:
+                heads += 1
+            else:
+                tails += 1
         else:
-            tails += 1
+            if has_changed(heads1x, heads2x, heads1y, heads2y):
+                heads += 1
+            else:
+                tails += 1
         flips -= 1
     elif int(choice) == 1 or int(choice) == 2:
         if has_changed(score1x, score2x, score1y, score2y):
             flips += 15
         else:
             flips -= 30
-
+        heads = 0
+        tails = 0
+        just_accused = 2
+    just_accused -= 1
     print(heads, tails, flips)
